b2f518c50ccc41e14333e25e0d575145e7b4361c
Ivan Ferro <ivanferroherbon@gmail.com>
2021-01-14 20:48:11 +0100

    WIP: dockerfile + docker-compose

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -2,5 +2,5 @@
 pushd ../
 export SOFTWARE_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" "pom.xml")
 
-docker build -f docker/Dockerfile -t openvidu/openvidu-classroom-demo .
-docker tag openvidu/openvidu-classroom:latest openvidu/openvidu-classroom-demo:$SOFTWARE_VERSION
+docker build -f docker/Dockerfile -t openvidu/openvidu-smmc .
+docker tag openvidu/openvidu-smmc:latest openvidu/openvidu-smmc:$SOFTWARE_VERSION
