2deb38803e987345b5d4864055d40bd87f68d437
Ivan Ferro <ivanferroherbon@gmail.com>
2021-01-14 22:38:58 +0100

    WIP: docker image from dockerhub

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -2,5 +2,5 @@
 pushd ../
 export SOFTWARE_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" "pom.xml")
 
-docker build -f docker/Dockerfile -t openvidu/openvidu-smmc .
-docker tag openvidu/openvidu-smmc:latest openvidu/openvidu-smmc:$SOFTWARE_VERSION
+docker build -f docker/Dockerfile -t iivanf/openvidu-smmc .
+docker tag iivanf/openvidu-smmc:latest iivanf/openvidu-smmc:$SOFTWARE_VERSION
